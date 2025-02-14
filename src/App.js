425a272e5ee35acacd9c9e352019f92e79a4bc3e
thuann17 <thuannguyen35753@gmail.com>
2025-02-14 14:59:44 +0700

    Lần đầu của em

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-500">
+        Hello React + Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
