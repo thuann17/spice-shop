605de92d2be91bd8609f80ace9de58579f9835ea
HieuHuynh <thanhhieuhuynh030104@gmail.com>
2025-02-15 21:46:21 +0700

    SPA Body
    Đã gắn body và header

--- src/components/test.js
+++
@@ -1,7 +0,0 @@
-import icon from'../assets/images/icon.png'
-import icon2 from'../assets/images/z6318443437947_96cbb004097222a8ef9eef9b68114e3f.jpg'
-const Test = {
-    icon,
-    icon2
-}
-export default Test;
