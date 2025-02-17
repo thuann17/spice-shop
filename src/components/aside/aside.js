793538ad06c63cd7eba9e75f80092ca2a60c562a
HieuHuynh <thanhhieuhuynh030104@gmail.com>
2025-02-17 17:17:32 +0700

    chỉnh khoảng cách đều
    a

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Aside = () => {
     return (<>
-        <aside className="col-span-2 bg-button p-4 h-[400px] overflow-y-auto rounded-lg scrollbar scrollbar-thin scrollbar-thumb-white scrollbar-track-gray-300">
+        <aside className="col-span-2 bg-button p-4 h-[400px] overflow-y-auto rounded-lg scrollbar scrollbar-thin scrollbar-thumb-white scrollbar-track-gray-300 mt-6 mx-4">
             <ul className="space-y-2">
                 <li className="p-2 bg-secondary shadow-md rounded">Gia vị</li>
                 <li className="p-2 bg-secondary shadow-md rounded">Đóng chai</li>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,10 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-primary text-white text-center py-4 mt-4 rounded-lg mx-4">
-      Footer
+       
+         LOGO
+          
+       
     </footer>
   );
 };
