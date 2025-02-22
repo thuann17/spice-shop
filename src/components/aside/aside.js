0d88cdb5390aa63f2e43b252b3c26664549de3af
thuanwbit <117891819+thuann17@users.noreply.github.com>
2025-02-22 12:19:53 +0700

    fix aside

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -1,7 +1,5 @@
 import React from "react";
-import RangeSlider from "../range/index";
 import videoFile from "../../assets/images/6336741841146.mp4";
-
 
 const categories = ["Sốt và nước chấm", "Món ăn", "Dịch vụ lưu động"];
 
@@ -10,18 +8,16 @@ const Aside = ({ onSelectCategory }) => {
     <>
       <div className="col-span-3">
         <aside className="bg-button p-4 h-[400px] overflow-y-auto rounded-lg scrollbar scrollbar-thin scrollbar-thumb-white scrollbar-track-gray-300 mt-5 mx-4">
-        <video 
-          controls 
-          loop 
-          autoPlay 
-          muted 
-          className="w-full h-full rounded-lg shadow-lg"
-        >
-        <source src={videoFile} type="video/mp4" />
-
-        </video>
+          <video
+            controls
+            loop
+            autoPlay
+            muted
+            className="w-full h-full rounded-lg shadow-lg"
+          >
+            <source src={videoFile} type="video/mp4" />
+          </video>
         </aside>
-        
       </div>
     </>
   );
