4a0d67c952f6a4e0d659be3dd31e64172f6dc904
thuanwbit <117891819+thuann17@users.noreply.github.com>
2025-02-27 15:42:41 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,6 @@ import HomePage from "../pages/Home/HomePage";
 import CardList from "../pages/DichVuLuuDong/CardList";
 import CardList2 from "../pages/DichVuLuuDong/CardList2";
 
-
 const AppRouter = () => {
   return (
     <Router>
@@ -18,7 +17,7 @@ const AppRouter = () => {
         <Route path="menu-list1" element={<Menus1 />} />
         <Route path="menu-list2" element={<Menus2 />} />
         <Route path="menu-list3" element={<Menus3 />} />
-        <Route path="menu-list" element={<Menus />} />
+
         <Route path="services" element={<CardList />} />
         <Route path="services2" element={<CardList2 />} />
       </Routes>
